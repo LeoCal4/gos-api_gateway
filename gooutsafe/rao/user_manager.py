46a147fb93856aedfbabe4faee56d5540ca03ef9
Paolo Murgia <62264946+Musca23@users.noreply.github.com>
2020-11-19 19:50:52 +0100

    added function for positives (not finished)

--- gooutsafe/rao/user_manager.py
+++ gooutsafe/rao/user_manager.py
@@ -78,6 +78,24 @@ class UserManager:
         return user
 
     @classmethod
+    def get_all_positive_customer(cls) -> [User]:
+        """
+        This method contacts the users microservice
+        and retrieves all positive customers.
+        :return: A list of User obj with health_status = True
+        """
+
+        response = requests.get("%s/positive_customer" % cls.USERS_ENDPOINT)
+        json_payload = response.json()
+        pos_customers = []
+
+        if response.status_code == 200:
+            #TODO append in pos_customers all item of the json_payload
+            pass
+        
+        return None
+
+    @classmethod
     def add_social_number(cls, user_id: int, social_number:str):
         """
         This method contacts the users microservice
--- gooutsafe/views/auth.py
+++ gooutsafe/views/auth.py
@@ -125,8 +125,8 @@ def authority(id, positive_id):
     """
     if current_user.id == id:
         ha_form = AuthorityForm()
-        #TODO implement retrieving of all positive in rao and user microservice
-        #pos_customers = CustomerManager.retrieve_all_positive()
+        #TODO implement retrieving of all positive in rao and manage them for the properties
+        pos_customers = UserManager.get_all_positive_customer()
         pos_customers = None
         if positive_id != 0:
             search_customer = UserManager.get_user_by_id(positive_id)
