765a753a5ef0b83956d24066186651317e51221b
Federico Silvestri <silves.federico@gmail.com>
2020-11-22 23:36:07 +0100

    Cleaned code from typo and bad variable names

--- gooutsafe/rao/notification_tracing_manager.py
+++ gooutsafe/rao/notification_tracing_manager.py
@@ -2,18 +2,18 @@ from gooutsafe import app
 from flask import abort
 import requests
 
-class NotificationTracingManager:
 
-    NOTIFICATION_ENDPOINT = app.config['NOTIFICATION_MS_URL']
+class NotificationTracingManager:
+    NOTIFICATION_ENDPOINT = app.config['NOTIFICATIONS_MS_URL']
     REQUESTS_TIMEOUT_SECONDS = app.config['REQUESTS_TIMEOUT_SECONDS']
 
     @classmethod
-    def retrieve_by_target_user_id(cls,user_id: int):
+    def retrieve_by_target_user_id(cls, user_id: int):
         try:
-            response = requests.get("%s/notifications/%s" % (cls.NOTIFICATION_ENDPOINT, str(user_id)), 
-                        timeout=cls.REQUESTS_TIMEOUT_SECONDS)
+            response = requests.get("%s/notifications/%s" % (cls.NOTIFICATION_ENDPOINT, str(user_id)),
+                                    timeout=cls.REQUESTS_TIMEOUT_SECONDS)
             if response.status_code == 200:
-                #there are notifications
+                # there are notifications
                 return response.json()
             elif response.status_code == 404:
                 return []
@@ -24,12 +24,12 @@ class NotificationTracingManager:
             return abort(500)
 
     @classmethod
-    def get_contact_tracing_list(cls,customer_id: int):
+    def get_contact_tracing_list(cls, customer_id: int):
         try:
             response = requests.get("%s/contact_tracing/%s" % (cls.NOTIFICATION_ENDPOINT, str(customer_id)),
-                        timeout=cls.REQUESTS_TIMEOUT_SECONDS)
+                                    timeout=cls.REQUESTS_TIMEOUT_SECONDS)
             if response.status_code == 200:
-                #there are contacts
+                # there are contacts
                 return response.json()['tracing_list']
             elif response.status_code == 404:
                 return []
--- gooutsafe/rao/reservation_manager.py
+++ gooutsafe/rao/reservation_manager.py
@@ -4,8 +4,9 @@ from .restaurant_manager import RestaurantManager
 import requests
 from flask import abort
 
+
 class ReservationManager:
-    RESERVATION_ENDPOINT = app.config['RESERVATION_MS_URL']
+    RESERVATION_ENDPOINT = app.config['RESERVATIONS_MS_URL']
     REQUESTS_TIMEOUT_SECONDS = app.config['REQUESTS_TIMEOUT_SECONDS']
     """
     Reservation Manager RAO
@@ -13,23 +14,22 @@ class ReservationManager:
 
     @classmethod
     def create_reservation(cls, restaurant_id, user_id, start_time, people_number):
-        url = "%s/reservation/" % (cls.RESERVATION_ENDPOINT)
+        url = "%s/reservation/" % cls.RESERVATION_ENDPOINT
         json_tables, json_times, _ = cls.get_restaurant_detatils(restaurant_id)
         try:
             response = requests.post(url,
-                                    json={
-                                        'user_id': user_id,
-                                        'restaurant_id': restaurant_id,
-                                        'start_time': start_time,
-                                        'people_number': people_number, 
-                                        'tables': json_tables,
-                                        'times': json_times
-                                    },
-                                    timeout=cls.REQUESTS_TIMEOUT_SECONDS)
+                                     json={
+                                         'user_id': user_id,
+                                         'restaurant_id': restaurant_id,
+                                         'start_time': start_time,
+                                         'people_number': people_number,
+                                         'tables': json_tables,
+                                         'times': json_times
+                                     },
+                                     timeout=cls.REQUESTS_TIMEOUT_SECONDS)
         except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
             return abort(500)
         return response
-
 
     @classmethod
     def get_all_reservation_restaurant(cls, restaurant_id):
@@ -66,38 +66,38 @@ class ReservationManager:
             response = requests.put(url,
                                     json={
                                         'start_time': start_time,
-                                        'people_number': people_number, 
+                                        'people_number': people_number,
                                         'tables': json_tables,
                                         'times': json_times
                                     },
                                     timeout=cls.REQUESTS_TIMEOUT_SECONDS)
         except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
-            return abort(500)        
+            return abort(500)
         return response
 
     @classmethod
     def confirm_reservation(cls, reservation_id):
         url = "%s/reservation/confirm/%s" % (cls.RESERVATION_ENDPOINT, str(reservation_id))
         try:
-            response = requests.put(url,timeout=cls.REQUESTS_TIMEOUT_SECONDS)
+            response = requests.put(url, timeout=cls.REQUESTS_TIMEOUT_SECONDS)
         except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
-            return abort(500)        
+            return abort(500)
         return response
 
-    @classmethod    
+    @classmethod
     def filtered_reservations(cls, restaurant_id, start_time, end_time):
         url = "%s/reservation/dates/" % (cls.RESERVATION_ENDPOINT)
         try:
             response = requests.post(url,
-                                    json={
-                                        'restaurant_id': restaurant_id,
-                                        'start_time': start_time,
-                                        'end_time': end_time, 
-                                    },
-                                    timeout=cls.REQUESTS_TIMEOUT_SECONDS)
+                                     json={
+                                         'restaurant_id': restaurant_id,
+                                         'start_time': start_time,
+                                         'end_time': end_time,
+                                     },
+                                     timeout=cls.REQUESTS_TIMEOUT_SECONDS)
         except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
-            return abort(500)        
-        return response 
+            return abort(500)
+        return response
 
     @classmethod
     def delete_reservation(cls, reservation_id):
@@ -105,15 +105,15 @@ class ReservationManager:
         try:
             response = requests.delete(url, timeout=cls.REQUESTS_TIMEOUT_SECONDS)
         except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
-            return abort(500)       
+            return abort(500)
         return response
 
-# Helper Methods
+    # Helper Methods
     @classmethod
-    def get_restaurant_detatils(cls,restaurant_id):
-        json_details = {}            
+    def get_restaurant_detatils(cls, restaurant_id):
+        json_details = {}
         json_tables = {}
-        json_times = {}        
+        json_times = {}
         try:
             res = RestaurantManager.get_restaurant_sheet(restaurant_id)
             print(res)
@@ -123,4 +123,3 @@ class ReservationManager:
         except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
             return abort(500)
         return json_tables, json_times, json_details
-
