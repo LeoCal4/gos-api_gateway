b623b02d733b2c7cabdf291e0da0be75ac22584e
Claire-gip <chiara_boni@outlook.it>
2020-11-24 23:10:18 +0100

    Login test

--- tests/views/test_users.py
+++ tests/views/test_users.py
@@ -2,6 +2,7 @@ from unittest.mock import Mock, patch
 from .view_test import ViewTest
 from werkzeug.exceptions import HTTPException
 from flask import url_for
+from faker import Faker
 import requests
 
 
@@ -101,191 +102,115 @@ class TestUsers(ViewTest):
             assert len(templates) == 1
             template, _ = templates[0]
             assert template.name == 'create_user.html' 
-
+    
     def test_create_customer_get(self, mock_get):
-        user = self.generate_user(type='customer')
-        user_data = {
-            'id': user.id, 
-            'email': user.email,
-            'is_active' : user.is_active,
-            'authenticated': user.is_authenticated,
-            'is_anonymous': False,
-            'type': 'operator',
-        }
-        mock_get.return_value = Mock(status_code=200)
-        with self.captured_templates(self.app) as templates:
-            response = self.client.get(
-                '/create_user/'+ user.type,
-                follow_redirects=False
-            )
-
-            assert response.status_code == 200
-            assert len(templates) == 1
-            template, _ = templates[0]
-            assert template.name == 'create_user.html'
-
-    def test_delete_user(self, mock_delete):
-        user = self.generate_user(type='generic')
-        mock_delete.return_value = Mock(
-            status_code=202,
-            json =  lambda : {
-                'status': 'success',
-                'message': 'Successfully deleted'
-            }
-        )
-        with self.captured_templates(self.app) as templates:
-            response = self.client.post(
-                '/delete_user/'+str(user.id),
-                follow_redirects=False
-            )
-            assert response is not None
-            assert response.status_code == 302  
-
-    def test_update_customer_get(self, mock_put):
-        user = self.generate_user(type='customer')
-        mock_put.return_value = Mock(
-            status_code=204,
-            json=lambda:{
-                'status': 'success',
-                'message': 'Updated'
-            }
-        )
-        data = { 
-            'email': user.email, 
-            'password': self.faker.password(),
-            'phone': user.phone
-        }   
-        with self.captured_templates(self.app) as templates:
-            response = self.client.get(
-                '/update_customer/'+str(user.id),
-                json=data,
-                follow_redirects=False
-            )
-            assert response.status_code == 200
-            assert len(templates) == 1
-            template, _ = templates[0]
-            assert template.name == 'update_customer.html' 
-   
-    def test_update_customer_post(self, mock_put, mock_get):
-        user = self.generate_user(type='customer')
-        mock_put.return_value = Mock(
-            status_code=204,
-            json=lambda:{
-                'status': 'success',
-                'message': 'Updated'
-            }
-        )
-        user_data = {
-            'id':user.id,
-            'email':user.email,
-            'is_active': False,
-            'authenticated': False,
-            'is_anonymous': False,
-            'type': user.type
-        }
-        mock_get.return_value = Mock(
-            status_code=204,
-            json=lambda : user_data
-        )
-        data = { 
-            'email': user.email, 
-            'password': self.faker.password() ,
-            'phone': user.phone
-        }   
-        with self.captured_templates(self.app) as templates:
-            response = self.client.post(
-                '/update_customer/'+str(user.id),
-                json=data,
-                follow_redirects=False
-            )
-            assert response.status_code == 302
-
-    def test_update_customer_post_error(self, mock_put, mock_get):
-        user = self.generate_user(type='customer')
-        mock_put.return_value = Mock(
-            status_code=204,
-            json=lambda:{
-                'status': 'success',
-                'message': 'Updated'
-            }
-        )
-        user_data = {
-            'id':user.id,
-            'email':user.email,
-            'is_active': False,
-            'authenticated': False,
-            'is_anonymous': False,
-            'type': user.type
-        }
-        mock_get.return_value = Mock(
-            status_code=200,
-            json=lambda : user_data
-        )
-        
-        data = { 
-            'email': user.email, 
-            'password': self.faker.password(),
-            'phone': user.phone
-        }   
-        with self.captured_templates(self.app) as templates:
-            response = self.client.post(
-                '/update_customer/'+str(user.id +1),
-                json=data,
-                follow_redirects=False
-            )
-            assert response.status_code == 200
-            assert len(templates) == 1
-            template, _ = templates[0]
-            assert template.name == 'update_customer.html'
-    """
-    def test_update_operator_get(self, mock_put):
-        user = self.generate_user(user_type='operator')
-        #login
+        user = self.login_test_customer()
         data = {
             'email': user.email, 
-            'password': self.faker.password()
+            'password': user.password,
+            'phone': user.phone
         }
         rv = self.client.get(
-            self.BASE_URL+'/update_operator/'+str(user.id), 
+            self.BASE_URL+'/create_user/'+ user.type, 
             json=data, 
             follow_redirects=False
         )
+        assert response.status_code == 200
+    """
+    def test_delete_user(self):
+        user = self.login_test_customer()
+        rv = self.client.get(
+            self.BASE_URL+'/delete_user/'+str(user.get("id")), 
+            follow_redirects=False
+        )
         assert rv.status_code == 302
-
-    def test_update_operator_post(self):
-        user = self.generate_user(user_type='operator')
-        #login
+    
+    def test_update_customer_get(self):
+        user = self.login_test_customer()
         data = {
-            'email': user.email, 
-            'password': self.faker.password()
+            'email': user.get("email"), 
+            'password': user.get("password"),
+            'phone': user.get("phone")
+        }
+        rv = self.client.get(
+            self.BASE_URL+'/update_customer/'+str(user.get("id")), 
+            json=data,
+            follow_redirects=False
+        )
+        assert rv.status_code == 200
+    
+    def test_update_customer_post(self):
+        user = self.login_test_customer()
+        data = {
+            'email': user.get("email"), 
+            'password': user.get("password"),
+            'phone': user.get("phone")
         }
         rv = self.client.post(
-            self.BASE_URL+'/update_operator/'+str(user.id), 
+            self.BASE_URL+'/update_customer/'+str(user.get("id")), 
             json=data, 
             follow_redirects=False
         )
-        assert rv.status_code == 302
+        assert rv.status_code == 200
+
+    def test_update_customer_post_error(self):
+        user = self.login_test_customer()
+        data = {
+            'email': user.get("email"), 
+            'password': user.get("password"),
+            'phone': user.get("phone")
+        }
+        rv = self.client.post(
+            self.BASE_URL+'/update_customer/'+str(user.get("id")+1), 
+            json=data, 
+            follow_redirects=False
+        )
+        assert rv.status_code == 200
+    
+    def test_update_operator_get(self):
+        user = self.login_test_operator()
+        data = {
+            'email': user.get("email"), 
+            'password': user.get("password")
+        }
+        rv = self.client.get(
+            self.BASE_URL+'/update_operator/'+str(user.get("id")), 
+            json=data, 
+            follow_redirects=False
+        )
+        assert rv.status_code == 200
+    
+    def test_update_operator_post(self):
+        user = self.login_test_operator()
+        data = {
+            'email': user.get("email"), 
+            'password': user.get("password")
+        }
+        rv = self.client.post(
+            self.BASE_URL+'/update_operator/'+str(user.get("id")), 
+            json=data, 
+            follow_redirects=False
+        )
+        assert rv.status_code == 200
 
     def test_update_operator_post_error(self):
-        user = self.generate_user(user_type='operator')
-        #login
+        user = self.login_test_operator()
         data = {
-            'email': user.email, 
-            'password': self.faker.password()
+            'email': user.get("email"), 
+            'password': user.get("password")
         }
         response = self.client.post(
-            self.BASE_URL+'/update_operator/'+str(user.id +1),
+            self.BASE_URL+'/update_operator/'+str(user.get("id") +1),
             json=data,
             follow_redirects=False
         )
         assert response.status_code == 200
-
+    
     def test_add_social_number(self):
-        user = self.generate_user(user_type='customer')
-        #login
+        user = self.login_test_customer()
         rv = self.client.post(
-            self.BASE_URL+'/add_social_number/'+str(user.id), 
-            json={ 'social_number': user.social_number }, 
+            self.BASE_URL+'/add_social_number/'+str(user.get("id")), 
+            json={ 'social_number': user.get("social_number") },
             follow_redirects=False
         )
         assert rv.status_code == 302         
--- tests/views/view_test.py
+++ tests/views/view_test.py
@@ -18,39 +18,29 @@ class ViewTest(unittest.TestCase):
         cls.client = cls.app.test_client()
         from gooutsafe.rao.user_manager import UserManager
         cls.user_manager = UserManager 
-
-    @contextmanager
-    def captured_templates(self, app):
-        recorded = []
-        def record(sender, template, context, **extra):
-            recorded.append((template, context))
-        template_rendered.connect(record, app)
-        try:
-            yield recorded
-        finally:
-            template_rendered.disconnect(record, app)
     
+
     def login_test_customer(self):
         """
         Simulate the customer login for testing the views with @login_required
         :return: customer
         """
         customer = self.generate_user('customer')
-        # create customer
         response = self.user_manager.create_customer(
                 'customer',
                 customer.get('email'),
                 customer.get('password'),
-                customer.get('extra_data').get('social_number'),
-                customer.get('extra_data').get('firstname'),
-                customer.get('extra_data').get('lastname'),
-                customer.get('extra_data').get('date'),
-                customer.get('extra_data').get('phone')
+                customer.get('social_number'),
+                customer.get('firstname'),
+                customer.get('lastname'),
+                customer.get('birthdate'),
+                customer.get('phone')
                 )
-        user = response.json()
-        if user["status"] == "success":
-            to_login = User.build_from_json(user["user"])
-            login_user(to_login)
+
+        rv = self.client.post (
+            self.BASE_URL+'/login',
+            json=customer
+        )
         return customer
 
     def login_test_operator(self):
@@ -59,48 +49,65 @@ class ViewTest(unittest.TestCase):
         :return: operator
         """
         operator = self.generate_user('operator')
-        psw = operator.password
-        # create operator
         response = self.user_manager.create_operator(
-                operator.email,
-                operator.password,
+                operator.get('email'),
+                operator.get('password'),
                 )
-        user = response.json()
-        if user["status"] == "success":
-            to_login = User.build_from_json(user["user"])
-            login_user(to_login)
-        data = {'email': operator.email, 'password': psw}
-        return operator
 
+        rv = self.client.post (
+            self.BASE_URL+'/login',
+            json=operator
+        )
+        return operator
     
     def generate_user(self, user_type):
+        """Generates a random user, depending on the type
+
+        Args:
+            user_type (str): type of the user to be created
+
+        Returns:
+            (dict): a dictionary with the user's data
+        """
         if user_type == 'customer':
-            extra_data = {
-            'firstname': self.faker.first_name(),
-            'lastname': self.faker.last_name(),
-            'birthdate': self.faker.date(),
-            'social_number': self.faker.ssn(),
-            'health_status': choice([True, False]),
-            'phone': self.faker.phone_number()
+            data = {
+                'id': randint(0,999),
+                'email': self.faker.email(),
+                'password': self.faker.password(),
+                'is_active' : choice([True,False]),
+                'authenticated': False,
+                'is_anonymous': False,
+                'type': user_type,
+                'firstname': self.faker.first_name(),
+                'lastname': self.faker.last_name(),
+                'birthdate': self.faker.date(),
+                'health_status_change_datetime': self.faker.date(),
+                'social_number': self.faker.ssn(),
+                'health_status': choice([True, False]),
+                'phone': self.faker.phone_number()
             }
         elif user_type == 'authority':
-            extra_data = {
-            'name': self.faker.company(),
-            'city': self.faker.city(),
-            'address': self.faker.address(),
-            'phone': self.faker.phone_number()
+            data = {
+                'id': randint(0,999),
+                'email': self.faker.email(),
+                'password': self.faker.password(),
+                'is_active' : choice([True,False]),
+                'authenticated': False,
+                'is_anonymous': False,
+                'type': user_type,
+                'name': self.faker.company(),
+                'city': self.faker.city(),
+                'address': self.faker.address(),
+                'phone': self.faker.phone_number()
             }
         else:
-            extra_data = {}
-
-        data = {
-            'id': randint(0,999),
-            'email': self.faker.email(),
-            'password': self.faker.password(),
-            'is_active' : choice([True,False]),
-            'authenticated': False,
-            'is_anonymous': False,
-            'type': user_type,
-            'extra': extra_data,
-        }
+            data = {
+                'id': randint(0,999),
+                'email': self.faker.email(),
+                'password': self.faker.password(),
+                'is_active' : choice([True,False]),
+                'authenticated': False,
+                'is_anonymous': False,
+                'type': user_type
+            }
         return data
