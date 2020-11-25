0a013ac7077fae083aab7c2432fde07281305d4d
gasparax <nunziolopardo@live.it>
2020-11-25 23:52:13 +0100

    Fix

--- tests/views/test_reservation.py
+++ tests/views/test_reservation.py
@@ -149,7 +149,7 @@ class ReservationTest(ViewTest):
         day = self.faker.random_int(min=1, max=30)
         hour = self.faker.random_int(min=10, max=19)
         start_time = datetime.datetime(year=2020, month=month, day=day, hour=hour)
-        start_time_str = datetime.datetime.strftime(start_time, "%Y-%m-%d %H:%M:%S")
+        start_time_str = datetime.datetime.strftime(start_time, "%Y-%m-%dT%H:%M:%SZ")
         people_number = self.faker.random_int(min=1, max=4)
         response = self.reservation_manager.create_reservation(restaurant_id, user_id, start_time_str, people_number)
         return response, start_time, restaurant_id
